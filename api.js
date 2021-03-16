37f06148243829b4e5e189769a724510a4cc5519
alestor123 <alestoraldous@gmail.com>
2021-03-16 20:29:16 +0530

    added example code

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
 var bad = require('bad-words/lib/lang.json').words
 module.exports = msg => {
-if(bad.some(word => msg.content.includes(word))) msg.delete({timeout:2000,}) && msg.reply('Please dont use foul language')
+if(bad.some(word => msg.content.includes(word))) msg.delete({timeout:2000,}) && msg.reply('Please dont use foul language !!!!')
 }
